3560ec22030b96b684879005966faf6a06a0d2b7
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-09 17:24:26 +0530

    fix: whitespaces

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,6 +1,6 @@
 import { FaHeart } from 'react-icons/fa'
 import { Button } from '@/components/ui/button'
-function FavoriteToggleButton() {
+function FavoriteToggleButton() { 
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
