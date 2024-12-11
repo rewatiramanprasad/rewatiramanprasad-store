7d6908ce0ddc75dca10957618bed5c59ea23f7c1
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-11 23:24:15 +0530

    feat: added favorite toggle button

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,10 +1,15 @@
 import { FaHeart } from 'react-icons/fa'
 import { Button } from '@/components/ui/button'
-function FavoriteToggleButton() { 
-  return (
-    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
-      <FaHeart />
-    </Button>
-  )
+import { auth } from '@clerk/nextjs/server'
+import { CardSignInButton } from '../form/Button'
+import { fetchFavoriteId } from '@/utils/actions'
+import FavoriteToggleForm from './FavoriteToggleForm'
+
+async function FavoriteToggleButton({ productId }: { productId: string }) {
+  const { userId } = await auth()
+  if (!userId) return <CardSignInButton />
+  const favoriteId = await fetchFavoriteId({ productId })
+  return <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />
+
 }
 export default FavoriteToggleButton
