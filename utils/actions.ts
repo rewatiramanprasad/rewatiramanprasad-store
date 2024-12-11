67bb19ff579d48fa372bfb759c03b7edf1085f01
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-11 23:36:27 +0530

    refactor: added fav route

--- utils/actions.ts
+++ utils/actions.ts
@@ -212,3 +212,16 @@ export const toggleFavoriteAction = async (prevState: {
     return renderError(error)
   }
 }
+
+export const fetchUserFavorite = async () => {
+  const user = await getAuthUser()
+  const favorites = await db.favorite.findMany({
+    where: {
+      clerkId: user.id,
+    },
+    include: {
+      product: true,
+    },
+  })
+  return favorites
+}
