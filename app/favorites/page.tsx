29b124c07f96a5818a4a014d3ea0eac92fc4b61e
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-11 23:35:55 +0530

    feat: favorite route added

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import SectionTitle from '@/components/global/SectionTitle'
+import { fetchUserFavorite } from '@/utils/actions'
+import ProductsGrid from '@/components/products/ProductsGrid'
 
-function FavoritesPage() {
-  return <div>FavoritesPage</div>
+async function FavoritesPage() {
+  const favorites = await fetchUserFavorite()
+  if (favorites.length === 0)
+    return <SectionTitle text="You have no favorite yet" />
+  return (
+    <div>
+      <SectionTitle text="Favorites" />
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
+  )
 }
 
 export default FavoritesPage
