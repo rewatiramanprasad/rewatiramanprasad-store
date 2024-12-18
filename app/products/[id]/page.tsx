20891e21e276705b7458624824f06daccc3161ab
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-19 02:36:10 +0530

    fix: missing attributes

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -42,7 +42,7 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
             {dollarsAmount}
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-          <AddToCart />
+          <AddToCart productId={params.id} />
         </div>
       </div>
       <ProductReviews productId={params.id} />
