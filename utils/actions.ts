dd52ee1b96dd0cb23ea5e2b89e394e59fcf1c9a0
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-19 22:16:09 +0530

    refactor: createOrderAction api

--- utils/actions.ts
+++ utils/actions.ts
@@ -537,10 +537,19 @@ export const removeCartItemAction = async (
 
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser()
+  let orderId:null|string=null
+  let cartId:null|string=null
   try {
     const cart = await fetchOrCreateCart({
       userId: user.id,
       errorOnFailure: true,
+    })
+    cartId=cart.id
+    await db.order.deleteMany({
+      where:{
+        clerkId:user.id,
+        isPaid:false
+      }
     })
     const order = await db.order.create({
       data: {
@@ -553,15 +562,11 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
       },
     })
 
-    await db.cart.delete({
-      where: {
-        id: cart.id,
-      },
-    })
+    orderId=order.id
   } catch (error) {
     return renderError(error)
   }
-  redirect('/orders')
+  redirect(`/checkout?orderId=${orderId}&cartId=${cartId}`)
 }
 
 export const fetchUserOrders = async () => {
