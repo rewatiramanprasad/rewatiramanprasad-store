972d119aa34a761d89ced181c7f09874a2cd8c19
Raman Prasad <ramanprasad.0203@gmail.com>
2024-12-19 20:53:58 +0530

    feat: added table Loading

---
+++ app/orders/loading.tsx
@@ -0,0 +1,13 @@
+'use client'
+import LoadingTable from '@/components/global/LoadingTable'
+import React from 'react'
+
+function loading() {
+  return (
+    <>
+      <LoadingTable />
+    </>
+  )
+}
+
+export default loading
